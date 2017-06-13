c544613c2ed47e3f7d7dc796759e7be4385a4a09
IP <pavlo.igor@gmail.com>
2017-06-13 17:11:34 +0200

    Ng2 Fundamentasl
    Ng2

---
+++ app/events/event-thumbnail.component.ts
@@ -0,0 +1,40 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core'
+
+@Component({
+    selector: 'event-thumbnail',
+    template: `
+    <div class="well horwell thumbnail">
+        <h2>{{event?.name}}</h2>
+        <div>Date: {{event?.date}}</div>
+        <div [ngStyle]="getStartTimeStyle()" [ngSwitch]="event?.time">
+            Time: {{event?.time}}
+            <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+            <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+            <span *ngSwitchDefault>(Normal Start)</span>
+        </div>
+        <div>Price: \${{event?.price}}</div>
+        <div *ngIf="event?.location">
+            <span>Location: {{event?.location?.address}}</span>
+            <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
+        </div>
+        <div *ngIf="event?.onlineUrl">
+            Online URL: {{event?.onlineUrl}}
+        </div>
+    </div>
+    `,
+    styles: [`
+        .pad-left { margin-left: 10px; }
+        .well div { color: #bbb; }
+        .thumbnail { min-height: 210px; }
+    `]
+})
+export class EventThumbnailComponent {
+    @Input() event: any
+
+    getStartTimeStyle():any {
+        if (this.event && this.event.time === '8:00 am')
+            return {color:'#003300', 'font-weight': 'bold'}
+        return {}
+    }
+
+}       
---
+++ app/events/events-list.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from '@angular/core'
+import { EventService } from './shared/event.service'
+
+@Component({
+    selector: 'events-list',
+    template: `
+    <div>
+        <h1>Upcoming Angular 2 Events</h1>
+        <hr /> 
+        <div class="row">
+            <div class="col-md-5" *ngFor="let event of events">
+                <event-thumbnail [event]="event"></event-thumbnail>
+            </div>
+        </div>
+    </div>
+    `
+})
+export class EventsListComponent implements OnInit{
+    events:any[]
+
+    constructor(private eventService:EventService){
+    }
+
+    ngOnInit(){
+        this.events = this.eventService.getEvents()
+    }
+}
