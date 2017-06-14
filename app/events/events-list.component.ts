a62545efb04aed2e3ad5bd8cbf9c86d339beef4d
IP <pavlo.igor@gmail.com>
2017-06-14 13:11:33 +0200

    Added Router Navigation
    Added some routes for the pages

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -1,27 +1,31 @@
-import {Component, OnInit} from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { EventService } from './shared/event.service'
+import { ToastrService } from '../common/toastr.service'
 
 @Component({
-    selector: 'events-list',
     template: `
     <div>
         <h1>Upcoming Angular 2 Events</h1>
         <hr /> 
         <div class="row">
             <div class="col-md-5" *ngFor="let event of events">
-                <event-thumbnail [event]="event"></event-thumbnail>
+                <event-thumbnail (click)="handleThumbnailClick(event.name)" [event]="event"></event-thumbnail>
             </div>
         </div>
     </div>
     `
 })
-export class EventsListComponent implements OnInit{
-    events:any[]
+export class EventsListComponent implements OnInit {
+    events: any[]
 
-    constructor(private eventService:EventService){
+    constructor(private eventService: EventService, private toastr:ToastrService) {
     }
 
-    ngOnInit(){
+    ngOnInit() {
         this.events = this.eventService.getEvents()
+    }
+
+    handleThumbnailClick(eventName) {
+        this.toastr.success(eventName);
     }
 }
