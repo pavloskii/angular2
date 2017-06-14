c98662c908ae2b53f1e178284acc07bb942e2b46
IP <pavlo.igor@gmail.com>
2017-06-14 16:43:25 +0200

    So bug
    with Bug

--- app/app.module.ts
+++ app/app.module.ts
@@ -16,6 +16,7 @@ import { NavBarComponent } from './nav/navbar.component'
 import { ToastrService } from './common/toastr.service'
 import { appRoutes } from './routes'
 import { Error404Component } from './errors/404.component'
+import { AuthService } from './user/auth.service'
 
 
 @NgModule({
@@ -37,7 +38,8 @@ import { Error404Component } from './errors/404.component'
         ToastrService,
         EventRouteActivator,
         { provide: 'canDeactivateCreateEvent', useValue: checkDirtyState },
-        EventsListResolver
+        EventsListResolver,
+        AuthService
     ],
     bootstrap: [EventsAppComponent]
 })
--- app/user/login.component.ts
+++ app/user/login.component.ts
@@ -1,8 +1,16 @@
-import { Component} from '@angular/core'
+import { Component } from '@angular/core'
+import { AuthService } from './auth.service'
+import { Router } from '@angular/router'
 
 @Component({
     templateUrl: 'app/user/login.component.html'
 })
-export class LoginComponent{
+export class LoginComponent {
+    constructor(private authService: AuthService) {
 
+    }
+
+    login(formValues) {
+        this.authService.loginUser(formValues.userName, formValues.password)
+    }
 }
