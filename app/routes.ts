362d45432d9e9d0ccf6346d199052ccea41e611a
IP <pavlo.igor@gmail.com>
2017-06-15 15:02:22 +0200

    Sessions now appear
    sessions

--- app/routes.ts
+++ app/routes.ts
@@ -1,18 +1,20 @@
 import { Routes } from '@angular/router'
-import{
-EventDetailsComponent,
-EventsListComponent,
-CreateEventComponent,
-EventRouteActivator,
-EventsListResolver
-}from './events/index'
+import {
+    EventDetailsComponent,
+    EventsListComponent,
+    CreateEventComponent,
+    EventRouteActivator,
+    EventsListResolver,
+    CreateSessionComponent
+} from './events/index'
 import { Error404Component } from './errors/404.component'
 
 export const appRoutes: Routes = [
     { path: 'events/new', component: CreateEventComponent, canDeactivate: ['canDeactivateCreateEvent'] },
     { path: 'events', component: EventsListComponent, resolve: { events: EventsListResolver } },
     { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator] },
+    { path: 'events/session/new', component: CreateSessionComponent },
     { path: '404', component: Error404Component },
     { path: '', redirectTo: '/events', pathMatch: 'full' },
-    { path: 'user', loadChildren: 'app/user/user.module#UserModule'}
+    { path: 'user', loadChildren: 'app/user/user.module#UserModule' }
 ]
